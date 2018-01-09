8b702d31f80dc53bfcfca6a95e040898f220d4f6
Unknown <eolsurion369@hotmail.com>
2018-01-08 16:00:58 -0800

    Migration of project into repo

---
+++ WebApplication4/Models/LibraryModels/MediaTypeGenre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication4.Models.LibraryModels
+{
+    public partial class MediaTypeGenre
+    {
+        public string MediaTypeId { get; set; }
+        public string GenreId { get; set; }
+
+        public Genre Genre { get; set; }
+        public MediaType MediaType { get; set; }
+    }
+}
